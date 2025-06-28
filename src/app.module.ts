127de42407ef48a612493d6a7823bca51cc26018
neto <picilianovasconcelos@gmail.com>
2025-06-28 14:33:35 -0300

    feat(app): configure main AppModule with core feature modules
    - Imported PrismaModule for database access throughout the app
    - Included UserModule to manage user-related functionality
    - Added HashModule to handle hashing logic securely
    - Integrated AuthModule to provide authentication features
    - Centralized essential modules in AppModule to bootstrap the NestJS application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from 'prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { HashModule } from './hash/hash.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UserModule, HashModule, AuthModule],
 })
 export class AppModule {}
