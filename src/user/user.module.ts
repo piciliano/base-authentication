fd14c794980c9c4667653ae76172775b18ea842f
neto <picilianovasconcelos@gmail.com>
2025-06-28 21:01:36 -0300

    chore(user): inject AuthModule and JwtModule into UserModule
    - Imported AuthModule to access AuthService for authentication-related logic
    - Imported JwtModule to enable usage of JwtService where needed
    - Prepared UserModule for secure operations involving token handling or user auth

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '../auth/jwt/jwt.module';
 
 @Module({
+  imports: [AuthModule, JwtModule],
   controllers: [UserController],
   providers: [UserService],
 })
